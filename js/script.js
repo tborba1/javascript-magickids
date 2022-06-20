8fe0fee6e8f1abc09fdc69b2d49a42ca069025e6
tborba1 <taryn.borba@gmail.com>
2022-06-20 15:34:07 -0700

    first commit

---
+++ js/script.js
@@ -0,0 +1,38 @@
+const answers = {
+    a: "apple",
+    b: "balloon",
+    c: "cat",
+    d: "dog",
+    e: "elephant",
+    f: "fish",
+    g: "giraffe",
+    h: "house",
+    i: "ice cream",
+    j: "jelly bean",
+    k: "key",
+    l: "lollipop",
+    m: "monkey",
+    n: "newt",
+    o: "octopus",
+    p: "pig",
+    q: "queen",
+    r: "rocket",
+    s: "sun",
+    t: "turtle",
+    u: "umbrella",
+    v: "volcano",
+    w: "whale",
+    x: "xylophone",
+    y: "yellow",
+    z: "zoo"
+  };
+  
+  // console.log(Object.keys(answers));
+  // console.log(Object.values(answers));
+  // console.log(answers.a);
+  
+  let let1 = prompt(`Enter a letter`);
+  let letter = let1.toLowerCase();
+  console.log(letter);
+  console.log(answers[letter]);
+  console.log(`${letter} = ${answers[letter]}`);
